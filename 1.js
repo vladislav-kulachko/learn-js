c8e5e18238b7f562d96c35599c9f35f2b8180cba
Vladislav Kulachko <76500070+vladislav-kulachko@users.noreply.github.com>
2021-04-02 06:23:10 +0300

    01.04_6:22

--- 1.js
+++ 1.js
@@ -1,3 +1,34 @@
-let a = Math.PI;
-console.log(a.toFixed(2));
-console.log(typeof a);
+let message;
+let D;
+
+let question = confirm((message = "Хотите вычислить обьем?"));
+
+if (question === true) {
+  D = prompt((message = "Введите диаметр"));
+} else if (question === false) {
+  question = confirm(
+    (message =
+      "Неужели он настолько мал что вы дрожите, осознавая результат;)???")
+  );
+}
+do {
+  if (question === false) {
+    D = prompt((message = "Введите диаметр"));
+  }
+
+  if (D === null) {
+    question = confirm(
+      (message =
+        "Неужели он настолько мал что вы дрожите, осознавая результат;)???")
+    );
+  }
+  if (question === true) {
+    break;
+  }
+} while (D === null);
+
+const circleArea = (Math.PI * D ** 2) / 4;
+console.log(circleArea.toFixed(2));
+console.log(typeof circleArea);
+console.log(D);
+console.log(question);
