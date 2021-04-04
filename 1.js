bb10ff1000ae6ae7d6bfb5731c013990bed15ba3
Vladislav Kulachko <vladcoool@gmail.com>
2021-04-04 05:12:19 +0300

    черновики 2 модуля, 23 задания

--- 1.js
+++ 1.js
@@ -1,34 +1,44 @@
 let message;
 let D;
-
+let L;
 let question = confirm((message = "Хотите вычислить обьем?"));
+
+function sizeFnc(D, L) {
+  const size = ((Math.PI * D ** 2) / 4) * L;
+  return size;
+}
 
 if (question === true) {
   D = prompt((message = "Введите диаметр"));
+  L = prompt((message = "Введите длинну"));
+  sizeFnc(D, L);
 } else if (question === false) {
   question = confirm(
-    (message =
-      "Неужели он настолько мал что вы дрожите, осознавая результат;)???")
+    (message = "Неужели он настолько мал что вы боитесь узнать результат;)???")
   );
 }
+
 do {
   if (question === false) {
     D = prompt((message = "Введите диаметр"));
+    L = prompt((message = "Введите длинну"));
+    sizeFnc(D, L);
   }
 
-  if (D === null) {
+  if (D === null || L === null) {
     question = confirm(
       (message =
-        "Неужели он настолько мал что вы дрожите, осознавая результат;)???")
+        "Неужели он настолько мал что вы боитесь узнать результат;)???")
     );
   }
   if (question === true) {
     break;
   }
-} while (D === null);
+} while (D === null || L === null);
 
-const circleArea = (Math.PI * D ** 2) / 4;
-console.log(circleArea.toFixed(2));
-console.log(typeof circleArea);
+console.log(sizeFnc(D, L).toFixed(1));
+// const size = ((Math.PI * D ** 2) / 4) * L;
+// console.log(size.toFixed(2));
 console.log(D);
+console.log(L);
 console.log(question);
